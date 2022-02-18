e7a824de9d2044ae968a7431aa5be8c01c39b5fa
cbpark68 <cbpark68@gmail.com>
2022-02-18 16:30:43 +0900

    gitignore테스트

--- test1/src/main/java/egovframework/example/sample/web/DeptController.java
+++ test1/src/main/java/egovframework/example/sample/web/DeptController.java
@@ -18,6 +18,9 @@ public class DeptController {
 		System.out.println("부서번호 : "+vo.getDeptno());
 		System.out.println("부서이름 : "+vo.getDname());
 		System.out.println("부서위치 : "+vo.getLoc());
+		System.out.println("부서번호 : "+vo.getDeptno());
+		System.out.println("부서이름 : "+vo.getDname());
+		System.out.println("부서위치 : "+vo.getLoc());
 		return "";
 	}
 }
