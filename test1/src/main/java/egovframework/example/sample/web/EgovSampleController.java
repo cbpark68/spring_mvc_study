8aeb246daf37be106ba1d28ce9d1c51de95daf47
cbpark68 <cbpark68@gmail.com>
2022-02-18 15:11:05 +0900

    test1 학습종료

--- test1/src/main/java/egovframework/example/sample/web/EgovSampleController.java
+++ test1/src/main/java/egovframework/example/sample/web/EgovSampleController.java
@@ -69,6 +69,15 @@ public class EgovSampleController {
 	@Resource(name = "beanValidator")
 	protected DefaultBeanValidator beanValidator;
 
+	@RequestMapping("/test1.do")
+	public String test1() {
+		return "sample/test1";
+	}
+	
+	@RequestMapping("/cal.do")
+	public String cal() {
+		return "sample/cal";
+	}
 	/**
 	 * 글 목록을 조회한다. (pageing)
 	 * @param searchVO - 조회할 정보가 담긴 SampleDefaultVO
