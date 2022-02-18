9ab9bbbd0634f9147c298b4cfccfa7f213ca696d
cbpark68 <cbpark68@cbpark68-virtual-machine>
2022-02-18 16:35:29 +0900

    우분투에서 gitignore 확인 후 커밋

--- test1/src/main/java/egovframework/example/sample/web/DeptController.java
+++ test1/src/main/java/egovframework/example/sample/web/DeptController.java
@@ -18,9 +18,6 @@ public class DeptController {
 		System.out.println("부서번호 : "+vo.getDeptno());
 		System.out.println("부서이름 : "+vo.getDname());
 		System.out.println("부서위치 : "+vo.getLoc());
-		System.out.println("부서번호 : "+vo.getDeptno());
-		System.out.println("부서이름 : "+vo.getDname());
-		System.out.println("부서위치 : "+vo.getLoc());
 		return "";
 	}
 }
