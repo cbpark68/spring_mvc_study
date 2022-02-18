5c5827149430cbe1050fb794d1c88f9d71b7ce09
cbpark68 <cbpark68@gmail.com>
2022-02-18 18:18:41 +0900

    gitignore.io 테스트

--- test1/src/main/java/egovframework/example/sample/web/DeptController.java
+++ test1/src/main/java/egovframework/example/sample/web/DeptController.java
@@ -16,8 +16,8 @@ public class DeptController {
 	@RequestMapping("/deptWriteSave.do")
 	public String deptWriteSave(DeptVO vo) {
 		System.out.println("부서번호 : "+vo.getDeptno());
-		System.out.println("부서이름 : "+vo.getDname());
-		System.out.println("부서위치 : "+vo.getLoc());
+//		System.out.println("부서이름 : "+vo.getDname());
+//		System.out.println("부서위치 : "+vo.getLoc());
 		return "";
 	}
 }
