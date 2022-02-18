4669a987dc238ba148b5772f9aff38868eb20449
cbpark68 <cbpark68@gmail.com>
2022-02-18 16:27:13 +0900

    ignore작동여부 테스트

--- test1/src/main/java/egovframework/example/sample/web/DeptController.java
+++ test1/src/main/java/egovframework/example/sample/web/DeptController.java
@@ -17,6 +17,7 @@ public class DeptController {
 	public String deptWriteSave(DeptVO vo) {
 		System.out.println("부서번호 : "+vo.getDeptno());
 		System.out.println("부서이름 : "+vo.getDname());
+		System.out.println("부서위치 : "+vo.getLoc());
 		return "";
 	}
 }
